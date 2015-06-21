50346aa06e73180a13cd2d6f4ccf91b6a0a6e9e4
dragonpt <cesar.maximo@gmail.com>
2015-06-21 01:48:31 +0100

    darkmoon: Swith to wiko darkmoon vendor

---
+++ darkmoon-vendor.mk
@@ -0,0 +1,12 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/darkmoon/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/darkmoon/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/darkmoon/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/darkmoon/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/darkmoon/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/darkmoon/gapps/system,system)
+
